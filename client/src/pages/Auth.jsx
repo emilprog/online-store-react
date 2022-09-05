3540a44d23f02b74ff9af090562f6312fcc4dbc5
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2022-09-05 13:22:55 +0300

    add pages

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Container, Form, Card, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+
+const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname == LOGIN_ROUTE
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: "600px"}}>
+                <Form className="d-flex flex-column m-5">
+                    <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        {isLogin ? "Войти" : "Регистрация"}
+                    </Button>
+                    <Row className="mt-3">
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
+    );
+};
+
+export default Auth;
---
+++ client/src/pages/Shop.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+
+const Shop = () => {
+    return (
+        <Container>
+            <Row>
+                <Col md={3}>
+                    
+                </Col>
+                <Col md={9}>
+
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Shop;
