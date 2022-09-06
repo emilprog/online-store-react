eb43cb3542175d7abf6c1466e60db6492826a2f3
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2022-09-06 13:21:10 +0300

    upd DeviceStore

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,18 +4,26 @@ class DeviceStore{
     constructor() {
         this._types=[
             {id: 1, name: 'Ноутбуки'},
-            {id: 1, name: 'Перфораторы'}
+            {id: 2, name: 'Перфораторы'},
+            {id: 3, name: 'Телефоны'},
+            {id: 4, name: 'Телевизоры'}
         ]
         this._brands=[
             {id: 1, name: 'MSI'},
-            {id: 1, name: 'Apple'}
+            {id: 2, name: 'Apple'},
+            {id: 3, name: 'Xiaomi'},
+            {id: 4, name: 'Asus'},
         ]
         this._devices=[
             {id: 1, name: 'MSI Topchik', price: 90000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'},
             {id: 2, name: 'MSI Kot', price: 10000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'},
             {id: 3, name: 'iPerfarator Man', price: 15000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'},
-            {id: 4, name: 'iPerfecto', price: 31000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'}
+            {id: 4, name: 'iPerfecto', price: 31000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'},
+            {id: 5, name: 'iPerfecto', price: 31000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'},
+            {id: 6, name: 'iPerfecto', price: 31000, raiting: 5, img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'}
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -31,14 +39,28 @@ class DeviceStore{
         this._devices = devices
     }
 
-    get Types(){
+    setSelectedType(type){
+        this._selectedType = type
+    }
+
+    setSelectedBrand(brand){
+        this._selectedBrand = brand
+    }
+
+    get types(){
         return this._types
     }
-    get Brands(){
+    get brands(){
         return this._brands
     }
-    get Devices(){
+    get devices(){
         return this._devices
+    }
+    get selectedType(){
+        return this._selectedType
+    }
+    get selectedBrand(){
+        return this._selectedBrand
     }
 }
 
