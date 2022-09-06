7e1e5e498a9e5698bfe0fa57598b0ff48944e29e
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2022-09-06 13:18:43 +0300

    Add NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,20 +1,33 @@
 import React, {useContext} from 'react';
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {Context} from "../index";
-import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {NavLink, useNavigate} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const navigate = useNavigate()
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: "white"}} to={SHOP_ROUTE}>НеТехника</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto">
-                        <Button variant={"outline-light"} onClick={() => user.setAuth(false)}>Выход</Button>
-                        <Button variant={"outline-light"} className="ms-2">Админ панель</Button>
+                        <Button
+                            variant={"outline-light"}
+                            // onClick={() => navigate(LOGIN_ROUTE)}
+                            onClick={() => user.setAuth(false)}
+                        >
+                            Выйти
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(ADMIN_ROUTE)}
+                            className="ms-2"
+                        >
+                            Админ панель
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto">
