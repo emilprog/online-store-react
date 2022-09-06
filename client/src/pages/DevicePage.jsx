01dd91980d18327efec95429c059206d45ac6190
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2022-09-06 13:19:44 +0300

    add device page

--- client/src/pages/DevicePage.jsx
+++ client/src/pages/DevicePage.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 
 const DevicePage = () => {
+    const device = {
+        id: 1,
+        name: 'MSI Topchik',
+        price: 90000,
+        raiting: 5,
+        img: 'https://static.1k.by/images/productsimages/ip/big/pp0/a/4604449/i78c456cf4.jpg'
+    }
+
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className={"mt-3"}>
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row>
+                        <h2>{device.name}</h2>
+                        <div>Рейтинг: {device.raiting}</div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column justify-content-around align-items-center"
+                        style={{width: 300, height: 300, }}
+                    >
+                       <h3>{device.price} руб.</h3>
+                        <Button variant="primary">Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
